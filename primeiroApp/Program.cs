1aee53fe6cd1044414acba4105593e2b5f554fa0
BeatrizFS <beatrizsilva.sms@gmail.com>
2021-09-15 08:44:29 -0300

    first commit

---
+++ primeiroApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace primeiroApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Beatriz!");
+        }
+    }
+}
